945f6d649cb82a8e1c1e49c3af7a43e7e4adc9f8
Josher Slebodnik <josh.slebodnik@forumcomm.com>
2015-01-08 16:44:48 -0600

    Project

---
+++ Josher's Playground/Josher's Playground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Josher's Playground
+//
+//  Created by Josher Slebodnik on 1/8/15.
+//  Copyright (c) 2015 Josher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Josher's Playground/Josher's Playground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Josher's Playground
+//
+//  Created by Josher Slebodnik on 1/8/15.
+//  Copyright (c) 2015 Josher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
